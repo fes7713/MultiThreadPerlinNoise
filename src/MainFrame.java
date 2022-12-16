b048475a71a3af272dd791afe47736d07afe5965
Keita Nakashima <tun05036@temple.edu>
2022-12-16 09:08:38 +0900

    Initial Commit

---
+++ src/MainFrame.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class MainFrame {
+    public static void main(String[] argv)
+    {
+        System.out.println("Print");
+        FastNoise fn = new FastNoise();
+        fn.SetNoiseType(FastNoise.NoiseType.CubicFractal);
+        fn.SetInterp(FastNoise.Interp.Quintic);
+        double noise = fn.GetNoise(0, 0);
+        System.out.println(noise);
+
+        JFrame frame = new JFrame("New Frame");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(0, 0, 500, 500);
+        frame.setVisible(true);
+
+    }
+
+}
