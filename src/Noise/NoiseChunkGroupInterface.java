135e2c32b15d8e7afddaa48ff1b205571607fc2a
Keita Nakashima <tun05036@temple.edu>
2022-12-21 03:20:08 +0900

    Deleted push chunk functions

--- src/Noise/NoiseChunkGroupInterface.java
+++ src/Noise/NoiseChunkGroupInterface.java
@@ -3,14 +3,6 @@ package Noise;
 public interface NoiseChunkGroupInterface {
     void loadChunks(int chunkX, int chunkY);
 
-    NoiseChunkInterface[][] pushLeft(NoiseChunkGroup ncg, int length);
-    NoiseChunkInterface[][] pushLeft(NoiseChunkInterface[][] pushedChunks);
-    NoiseChunkInterface[][] pushTop(NoiseChunkGroup ncg, int length);
-    NoiseChunkInterface[][] pushTop(NoiseChunkInterface[][] pushedChunks);
-    NoiseChunkInterface[][] pushRight(NoiseChunkGroup ncg, int length);
-    NoiseChunkInterface[][] pushRight(NoiseChunkInterface[][] pushedChunks);
-    NoiseChunkInterface[][] pushBottom(NoiseChunkGroup ncg, int length);
-    NoiseChunkInterface[][] pushBottom(NoiseChunkInterface[][] pushedChunks);
     int getChunkWidth();
     int getChunkHeight();
 }
