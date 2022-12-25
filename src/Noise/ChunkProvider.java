90f140069a32d07e616f4d7f75d2567bc9b15787
Keita Nakashima <tun05036@temple.edu>
2022-12-26 02:34:50 +0900

    KeeperClass moved

--- src/Noise/ChunkProvider.java
+++ src/Noise/ChunkProvider.java
@@ -5,81 +5,6 @@ import java.util.Map;
 import java.util.Stack;
 
 public class ChunkProvider {
-    private class ReusableChunkKeeper
-    {
-        private final Map<Long, NoiseChunkInterface> chunkMap;
-        private final Stack<NoiseChunkInterface> chunkStack;
-
-        public ReusableChunkKeeper() {
-            chunkMap = new HashMap<>();
-            chunkStack = new Stack<>();
-        }
-
-        public void keepAllChunks(Map<Long, NoiseChunkInterface> map)
-        {
-            for(Map.Entry<Long, NoiseChunkInterface> entry: map.entrySet())
-            {
-                chunkMap.put(entry.getKey(), entry.getValue());
-                chunkStack.add(entry.getValue());
-            }
-            if(chunkStack.size() != chunkMap.size())
-                System.out.println("Errorrrr");
-        }
-
-        public void keepChunk(int col, int row, NoiseChunkInterface chunk)
-        {
-            long key = NoiseChunkInterface.getChunkKey(col, row);
-
-            if(chunkStack.contains(chunk))
-                throw new RuntimeException("Cannot keep the duplicate chunk");
-            chunkMap.put(key, chunk);
-            chunkStack.push(chunk);
-            if(chunkStack.size() != chunkMap.size())
-                System.out.println("Errorrrr");
-        }
-
-        public NoiseChunkInterface reuseChunk(int col, int row, float zoom)
-        {
-            long key = NoiseChunkInterface.getChunkKey(col, row);
-
-            if(chunkStack.isEmpty())
-            {
-                return null;
-            }
-
-            NoiseChunkInterface chunk = chunkMap.remove(key);
-            if(chunk != null) {
-                if(!chunkStack.remove(chunk))
-                    System.out.println("Error");
-            }
-            else
-            {
-                chunk = chunkStack.pop();
-                chunkMap.remove(chunk.getChunkKey());
-            }
-
-            chunk.reuseChunk(col, row, zoom);
-            if(chunkStack.size() != chunkMap.size())
-                System.out.println("Errorrrr");
-            return chunk;
-        }
-
-        public boolean isEmpty()
-        {
-            if(chunkStack.isEmpty() != chunkMap.isEmpty())
-                throw new RuntimeException("Chunk keeper has issue with keeping chunks. Chunks do not match");
-            return chunkStack.isEmpty();
-        }
-
-        public void clear()
-        {
-            chunkMap.clear();
-            chunkStack.clear();
-        }
-
-
-    }
-
     private static final ChunkProvider provider = new ChunkProvider();
 
     private final FastNoise fn;
@@ -175,4 +100,61 @@ public class ChunkProvider {
         clearMap(true);
         this.zoom = zoom;
     }
+
+    private static class ReusableChunkKeeper
+    {
+        private final Map<Long, NoiseChunkInterface> chunkMap;
+        private final Stack<NoiseChunkInterface> chunkStack;
+
+        public ReusableChunkKeeper() {
+            chunkMap = new HashMap<>();
+            chunkStack = new Stack<>();
+        }
+
+        public void keepAllChunks(Map<Long, NoiseChunkInterface> map)
+        {
+            for(Map.Entry<Long, NoiseChunkInterface> entry: map.entrySet())
+            {
+                chunkMap.put(entry.getKey(), entry.getValue());
+                chunkStack.add(entry.getValue());
+            }
+        }
+
+        public NoiseChunkInterface reuseChunk(int col, int row, float zoom)
+        {
+            long key = NoiseChunkInterface.getChunkKey(col, row);
+
+            if(chunkStack.isEmpty())
+            {
+                return null;
+            }
+
+            NoiseChunkInterface chunk = chunkMap.remove(key);
+            if(chunk != null) {
+                chunkStack.remove(chunk);
+            }
+            else
+            {
+                chunk = chunkStack.pop();
+                chunkMap.remove(chunk.getChunkKey());
+            }
+
+            chunk.reuseChunk(col, row, zoom);
+            return chunk;
+        }
+
+        public boolean isEmpty()
+        {
+            if(chunkStack.isEmpty() != chunkMap.isEmpty())
+                throw new RuntimeException("Chunk keeper has issue with keeping chunks. Chunks do not match");
+            return chunkStack.isEmpty();
+        }
+
+        public void clear()
+        {
+            chunkMap.clear();
+            chunkStack.clear();
+        }
+    }
+
 }
